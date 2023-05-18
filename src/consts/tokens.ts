4c29bc26f2758ccb7c47ecf8acbeb8ef87166e05
Shree Vatsa <vastas2002@gmail.com>
2023-05-18 20:30:55 +0530

    test

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -1,16 +1,15 @@
-import { WarpTokenConfig } from '../features/tokens/types';
+import { TokenMetadata } from '../features/tokens/types';
 
-export const tokenList: WarpTokenConfig = [
+export const tokenList: TokenMetadata[] = [
   // Example collateral token
   {
-    type: 'collateral',
-    chainId: 5,
-    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
-    hypCollateralAddress: '0x145de8760021c4ac6676376691b78038d3DE9097',
-    name: 'Weth',
-    symbol: 'WETH',
-    decimals: 18,
-    logoURI: '/logos/weth.png', // See public/logos/
+    "chainId": 80001,
+    "address": "0x4D86353d21Ae2898c868C579E81fc64e004ca1ca",
+    "hypCollateralAddress": "0xC9b23F2F949d3427Cce22e67A6E2e82fE06d1e00",
+    "name": "MOCKUSD",
+    "symbol": "USDC",
+    "decimals": 18,
+
   },
   // Example native token
   // {
